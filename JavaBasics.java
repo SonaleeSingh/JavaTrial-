4768439f13bb1253ea1dd45998188dfd9083f262
Sonalee Singh <sonaleesingh20@gmail.com>
2023-12-29 17:53:07 +0530

    first commint

---
+++ JavaBasics.java
@@ -0,0 +1,19 @@
+public class JavaBasics{
+    public static void main(String[] args) {
+        // System.out.print("HELLO RISHTA");
+
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        String name ="sonalee singh";
+        System.out.println(name);
+
+
+        
+        b=a;
+        System.out.println(a);
+    }
+}
+
+//boilerplate code
